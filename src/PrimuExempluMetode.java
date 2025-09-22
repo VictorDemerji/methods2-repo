fffb74e8daf2f45a96db3e61a5402762764746fb
VictorDemerji <demerji.v1@gmail.com>
2025-09-22 16:22:49 +0300

    Add new mesaj

---
+++ src/PrimuExempluMetode.java
@@ -0,0 +1,16 @@
+public class PrimuExempluMetode {
+    public static void main(String[] args) {
+        arataMesaj();
+        PrimulExempluMetode obiect = new PrimulExempluMetode();
+        obiect.arataMesaj2();
+    }
+
+    public static void arataMesaj() {
+        System.out.println("Mesaj de felicitare.");
+    }
+
+    public void arataMesaj2() {
+        System.out.println("Mesaj de felicitare.");
+    }
+
+}
